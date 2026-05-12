Fix score calculation and rounding in percentile script

pecentilenormal.py
@@ -56,8 +56,9 @@ if uploaded_file:
     )
 
     # Convert to 300 scale
-    df["Score"] = (
-        df["Raw_Total"] / 2,8
+    df["Score"] = np.round(
+    df["Raw_Total"] / 2,
+    8
     )
 
     # ==================================================